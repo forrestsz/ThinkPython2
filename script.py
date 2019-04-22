7cd2ef924437092e7ce42d40c35d1176175d60b1
Vincent <38312060+linqinghong@users.noreply.github.com>
2019-04-22 23:56:40 +0800

    Add files via upload

---
+++ script.py
@@ -0,0 +1,21 @@
+def first(word):
+    return word[0]
+
+def last(word):
+    return word[-1]
+
+def middle(word):
+    return word[1:-1]
+
+def is_palindrome(word):
+    if len(word) <= 1:
+        return True
+    if first(word) != last(word):
+        return False
+    return is_palindrome(middle(word))
+
+print(middle('aa'))
+print(middle('a'))
+print(middle(''))
+print(is_palindrome('allen'))
+print(is_palindrome('bob'))
